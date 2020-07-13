5c496b39c209424684d93fc1c890846602c0f64b
Mou <kathy.mou@ARSIAAM2AV2527E.local>
2020-07-13 14:06:51 -0500

    Updated some of the lines to correct the path for importing data, deleted unnecessary lines

--- scripts/FS9_OTUtable.R
+++ scripts/FS9_OTUtable.R
@@ -8,19 +8,8 @@
 #Clear workspace and load necessary packages
 rm(list=ls())
 
-#Set working directory (either on desktop or network drive)
-setwd("C:/Users/Kathy.Mou/Desktop/FSEP_Projects/FS9/FS9_RWorkspace")
-
 #Load library packages
 library(tidyverse)
-
-
-#Load image file
-load("FS9_OTU_table.RData")
-
-
-#Save image file
-save.image(file="FS9_OTU_table.RData")
 
 #########################################
 #Files needed:
@@ -29,7 +18,7 @@ save.image(file="FS9_OTU_table.RData")
 
 #Edit taxonomy file
 #Save "stability.outsingletons.abund.opti_mcc.0.03.cons.taxonomy" as a csv file in a spreadsheet editor
-taxonomy <- read.csv("stability.outsingletons.abund.opti_mcc.0.03.cons.taxonomy.csv")
+taxonomy <- read.csv("./data/stability.outsingletons.abund.opti_mcc.0.03.cons.taxonomy.csv")
 taxonomy
 taxonomy$Taxonomy <- gsub('*\\(.*?\\) *', '', taxonomy$Taxonomy) #Remove (100) from "Taxonomy" column
 taxonomy
@@ -38,7 +27,7 @@ write.csv(taxonomy, file = "FS9.taxonomy.csv") #In excel, remove the "Size" and 
 
 #Edit subsample.shared file
 #Save "stability.outsingletons.abund.opti_mcc.0.03.subsample.shared" as a csv file in a spreadsheet editor; remove "label", and "numOtus" rows
-shared <- read.csv("stability.outsingletons.abund.opti_mcc.0.03.subsample.shared.csv")
+shared <- read.csv("./data/stability.outsingletons.abund.opti_mcc.0.03.subsample.shared.csv")
 head(shared)
 shared[1:6,1]
 shared <- t(shared) #Transpose
@@ -47,12 +36,12 @@ shared[1:6,1]
 write.csv(shared, file = 'FS9.subsample.shared.csv') #Open in a spreadsheet editor and remove the "V*" row
 
 #Read new subsample.shared file and edit column name
-shared <- read.csv("FS9.subsample.shared.csv")
+shared <- read.csv("./data/FS9.subsample.shared.csv")
 head(shared)
 colnames(shared) [1] <- "OTU" #Rename first column to "OTU"
 
 #Read new taxonomy file and merge with 'shared' dataframe
-taxonomy <- read.csv("FS9.taxonomy.csv")
+taxonomy <- read.csv("./data/FS9.taxonomy.csv")
 OTUtable <- merge(shared, taxonomy, by.x ="OTU", by.y = "OTU") #Merge "FS9.subsample.shared.csv" and "FS9.taxonomy.csv via OTU"
 head(OTUtable)
 nrow(OTUtable) #1583
@@ -60,5 +49,5 @@ ncol(OTUtable) #170
 write.csv(OTUtable, file= "FS9.OTUtable.csv") #In a spreadsheet editor, open "FS9.OTUtable.csv" and remove first column (unnecessary)
 
 #check OTU table
-OTUtable <-read.csv("FS9.OTUtable.csv")
+OTUtable <-read.csv("./data/FS9.OTUtable.csv")
 head(OTUtable)
