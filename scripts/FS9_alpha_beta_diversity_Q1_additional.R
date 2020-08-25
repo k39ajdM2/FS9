f2fece14dbbf4e11f378143685781adc5a0b58c5
Mou <kathy.mou@ARSIAAM2AV2527E.local>
2020-08-25 14:15:39 -0500

    update scripts for Q1 additional analysis

--- scripts/FS9_alpha_beta_diversity_Q1_additional.R
+++ scripts/FS9_alpha_beta_diversity_Q1_additional.R
@@ -1,9 +1,9 @@
 #######################################################################
-#FS9 16S alpha and beta diversity - Noninfected vs Infected, all days
+#FS9 16S alpha and beta diversity - Noninfected vs Infected, additional analysis
 #Kathy Mou
 
 #NOTES: 
-#This code analyzes alpha and beta diversity statistics for fecal samples from all days; and associated plots
+#This code analyzes alpha and beta diversity statistics for fecal samples from all days or only days 4 and 7; and associated plots
 #This script uses files created in "FS9_phyloseq.R"
 
 #Clear workspace and load necessary packages
@@ -232,27 +232,27 @@ adon.good$p.adjusted2 <- round(adon.good$p.adjusted, 3) #Round p-values to 3 dec
 adon.good$p.adjusted2[adon.good$p.adjusted2 > 0.05] <- NA #For all p-values greater than 0.05, replace with "NA"
 write.csv(adon.good, file='FS9.WithinDayPairwiseComparisons.Q1.doubletons.alldays.txt', row.names=TRUE)
 
-#CONTINUE HERE
-#Try without days -3 and 0 to see if it'll change 
-
-
 #Alpha diversity
 #Calculating alpha diversity metrics: Shannon, Inverse Simpson
 meta$shannon <- diversity(otu) #"diversity" is a vegan function. The default index is set at "shannon". I added a shannon index column in 'meta'
 meta$invsimpson <- diversity(otu,index = 'invsimpson') #We used 'invsimpson' since it is easier to interpret than Simpson values and won't need to "inverse" the Simpson values to understand (With Simpson values, the lower the number, the higher the diversity)
 levels(sample_data(meta)$Day)
-meta$Day = factor(meta$Day, levels = c("DNEG3", "D0"))  # Set the level order of values in "Day" column
-levels(sample_data(meta)$Day) #"DNEG3" "D0"
+meta$Day = factor(meta$Day, levels = c("DNEG3", "D0", "D4", "D7"))  # Set the level order of values in "Day" column
+levels(sample_data(meta)$Day) #"DNEG3" "D0"  "D4"    "D7"  
 
 #Calculate the average shannon, invsimpson, numOTUs for each "All" subtype within meta
 shannon.invsimpson.numOTUs <- aggregate(meta[, 6:8], list(meta$All), mean)
 print(shannon.invsimpson.numOTUs)
 #Output (singletons removed):
 #        Group.1    numOTUS   shannon    invsimpson
-#1       D0_INF     84.92000  3.358896   16.38037
-#2    D0_NONINF     71.33333  3.089266   13.27419
-#3    DNEG3_INF     100.40741 3.895313   26.05418
-#4 DNEG3_NONINF     97.64706  3.864239   25.32537
+#1       D0_INFnm  83.00000 3.153554  15.628239
+2    D0_NONINFnm  71.33333 3.089266  13.274195
+3       D4_INFnm  79.66667 3.202696  11.903607
+4    D4_NONINFnm  77.60000 3.117897   9.115051
+5       D7_INFnm  94.80000 3.661901  19.286844
+6    D7_NONINFnm  92.37500 3.770617  18.321736
+7    DNEG3_INFnm 113.11765 4.062337  31.949254
+8 DNEG3_NONINFnm  97.64706 3.864239  25.325367
 write.csv(shannon.invsimpson.numOTUs, file="FS9.shannon.invsimpson.num.OTUs.doubletons.txt", row.names=TRUE)
 
 #Shannon
@@ -300,6 +300,196 @@ invsimp
 alphadiv <- plot_grid(shan, invsimp, labels = c('A', 'B'), label_size = 12) #How to fix plot dimensions so that y-axis labels doesn't overlap the plots?
 ggsave(alphadiv,
        filename = './results/alpha_diversity_NONINFvsINF.jpeg',
+       width = 300,
+       height = 120,
+       device = 'jpeg',
+       dpi = 300,
+       units = 'mm')
+
+
+
+
+
+###################################################################### Days 4, 7 only ##########
+#Setting up 'phyloseq' into dataframes for NMDS calculation
+meta <- data.frame(phyloseq.FS9@sam_data) #Make 'phyloseq.FS9' sam_data into dataframe
+otu <- data.frame((phyloseq.FS9@otu_table)) #Make 'phyloseq.FS9' otu_table into dataframe
+class(meta) #data.frame
+rownames(meta) == row.names(otu) #Make sure rownames between 'meta' and 'otu' match exactly. It is true
+meta$numOTUS <- rowSums(otu > 1) #For rows with sums greater than 1 in 'otu', move rows and their respective sum values into "numOTUs" column in 'meta'
+head(meta)
+
+#NMDS calculation (aka beta diversity)
+otu[1:10,1:10]
+dim(otu) #29 768 (doubletons removed)
+NMDS <- NMDS_ellipse(meta, otu, grouping_set = 'All')
+#Output:
+#[1] "Ordination stress: 0.171192923672683"
+
+#Separate meta data and ellipse data to two lists to make NMDS plot
+head(NMDS)
+metanmds <- NMDS[[1]] #'metanmds' has meta data + MDS calculations. Select this 1st list of 'NMDS' using double brackets
+df_ell <- NMDS[[2]] #'df_ell' is accessing 2nd list from 'NMDS' that has ellipse calculations
+
+#Need two separate lists for making NMDS plot
+df_ell$group
+head(df_ell)
+
+#Create "Day" and "Treatment" columns within 'df_ell' for faceting purposes
+df_ell <- df_ell %>% separate(group, into=c("Day","Treatment"), sep="_", remove=FALSE)
+View(df_ell)
+
+#Restructure level order for 'metanmds' and 'df_ell'
+unique(metanmds$Day) #"D4" "D7" 
+unique(df_ell$Day) #""D4"    "D7"
+metanmds$Day = factor(metanmds$Day, levels = c("D4", "D7"))
+df_ell$Day = factor(df_ell$Day, levels = c("D4", "D7"))
+levels(df_ell$Day) #"D4" "D7"
+levels(metanmds$Day) #"D4" "D7"
+dim(metanmds) #29 10
+dim(df_ell) #404 5
+
+#Creating NMDS day+treatment plot from NMDS calculations
+nmdsplot <- ggplot(data=metanmds, aes(x=MDS1, y=MDS2, color=Treatment)) + geom_point() + 
+  geom_segment(aes(x=MDS1, xend=centroidX, y=MDS2, yend=centroidY), alpha = 0.5) + 
+  geom_path(data=df_ell, aes(x=NMDS1, y=NMDS2, color=Treatment, group=group)) + 
+  facet_wrap(~Day, scales = 'free') +
+  #scale_color_brewer(palette="Dark2") +
+  theme_gray(base_size = 10) +
+  theme(strip.text.x = element_text(size=15), axis.text.x = element_text(size=13), axis.text.y = element_text(size=13), axis.title.x = element_text(size=14), axis.title.y = element_text(size=14), legend.text=element_text(size=14), legend.title=element_text(size=14)) +
+  labs(color="Treatment group")+
+  scale_color_manual(values = c(INFnm='#CC0066', NONINFnm='#56B4E9')) +
+  labs(caption = 'Ordination stress = 0.171')
+#nmdsplot2 <- nmdsplot + scale_colour_manual(values=c("#E69F00", "#56B4E9")) + theme(legend.position = "right")
+nmdsplot
+#Save 'nmdsplot' as a .tiff for publication, 500dpi
+#ggsave("NMDS_DayAndTreatment.tiff", plot=nmdsplot, width = 11, height = 5, dpi = 500, units =c("in"))
+
+#Creating NMDS day plot from NMDS calculations
+nmdsplot_day <- ggplot(data=metanmds, aes(x=MDS1, y=MDS2, color=Day)) + geom_point() + 
+  geom_segment(aes(x=MDS1, xend=centroidX, y=MDS2, yend=centroidY), alpha = .5) + 
+  geom_path(data=df_ell, aes(x=NMDS1, y=NMDS2, color=Day, group=group)) + 
+  labs(caption = 'Ordination stress = 0.171') 
+nmdsplot_day
+#Save 'nmdsplot_day' as a .tiff for publication, 500dpi
+#ggsave("NMDS_Day.tiff", plot=nmdsplot_day, width = 10, height = 6, dpi = 500, units =c("in"))
+
+#Creating NMDS treatment plot from NMDS calculations
+nmdsplot_treatment<- ggplot(data=metanmds, aes(x=MDS1, y=MDS2, color=Treatment)) + geom_point() + 
+  geom_segment(aes(x=MDS1, xend=centroidX, y=MDS2, yend=centroidY), alpha = .5) + 
+  geom_path(data=df_ell, aes(x=NMDS1, y=NMDS2, color=Treatment, group=group)) + 
+  scale_color_manual(values = c(INFnm='#CC0066', NONINFnm='#56B4E9')) +
+  labs(caption = 'Ordination stress = 0.171')  
+nmdsplot_treatment
+#Save 'nmdsplot_treatment' as a .tiff for publication, 500dpi
+#ggsave("NMDS_Treatment.tiff", plot=nmdsplot_treatment, width = 10, height = 6, dpi = 500, units =c("in"))
+
+#All points, all days, all treatments, samples that aren't relevant are grayed out on plot
+#Plotting with gridlines and axes, gray points for All days
+metanmds.2 <- metanmds
+metanmds.2$Treatment2 = metanmds.2$Treatment
+metanmds.2$Treatment2 <- as.character(metanmds.2$Treatment2)
+metanmds.2$Treatment2
+
+#All days and treatments faceted by day (gridlines)
+nmdsplot_treatment2<- ggplot(metanmds, aes(x=MDS1, y=MDS2)) +  annotate(x=metanmds.2$MDS1, y=metanmds.2$MDS2, color='grey57', geom = 'point')+
+  geom_path(data = df_ell, aes(x=NMDS1, y=NMDS2, color=Treatment), size=1.25) + 
+  geom_point(aes(color = Treatment), size = 2) + 
+  geom_segment(aes(x=MDS1, xend=centroidX, y=MDS2, yend=centroidY, color=Treatment), alpha=.5) + 
+  theme(panel.background = element_blank(),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(), 
+        panel.border = element_rect(fill = NA, color = 'grey57'),
+        axis.line = element_blank()) + facet_wrap(~Day) +
+  theme_bw() +
+  scale_color_manual(values = c(INFnm='#CC0066', NONINFnm='#56B4E9')) +
+  labs(caption = 'Ordination stress = 0.171', color="Treatment group")
+nmdsplot_treatment2
+#Save 'nmdsplot_treatment2' as a .tiff for publication, 500dpi
+#ggsave("NMDS_DayAndTreatment_AllSamples.tiff", plot=nmdsplot_treatment2, width = 10, height = 6, dpi = 500, units =c("in"))
+
+#Using pairwise.adonis function (beta diversity)
+adon <- pairwise.adonis(otu, meta$All) #Run pairwise.adonis on 'otu' OTU table and "All" column of 'meta' dataframe
+#adon contains all the pairwise comparisons
+adon$pairs #List all comparisons in the "pairs" column of 'nw.adon'
+goodcomps <- c(grep('D4_[A-Za-z]+ vs D4_[A-Za-z]+', adon$pairs),
+               grep('D7_[A-Za-z]+ vs D7_[A-Za-z]+', adon$pairs))
+# "[A-Za-z]" matches all capital and lowercase letters
+# "+" matches a whole word and not just one letter (if you didn't have "+", then it would match by one letter)
+# "c" creates the vector, lumps all pairs of specific groups of interest together
+# You want to make a vector to combine all the pairwise comparisons you're interested in (same day, different treatment group)
+adon.good <- adon[goodcomps,] #Rename 'goodcomps' vector to 'adon.good'
+adon.good
+adon.good$p.adjusted <- p.adjust(adon.good$p.value, method = 'fdr') #"p.adjust" function returns a set of p-values adjusted with "fdr" method
+adon.good$p.adjusted2 <- round(adon.good$p.adjusted, 3) #Round p-values to 3 decimal points and list in new "p.adjusted2" column
+adon.good$p.adjusted2[adon.good$p.adjusted2 > 0.05] <- NA #For all p-values greater than 0.05, replace with "NA"
+write.csv(adon.good, file='FS9.WithinDayPairwiseComparisons.Q1.doubletons.D4D7.txt', row.names=TRUE)
+
+#Alpha diversity
+#Calculating alpha diversity metrics: Shannon, Inverse Simpson
+meta$shannon <- diversity(otu) #"diversity" is a vegan function. The default index is set at "shannon". I added a shannon index column in 'meta'
+meta$invsimpson <- diversity(otu,index = 'invsimpson') #We used 'invsimpson' since it is easier to interpret than Simpson values and won't need to "inverse" the Simpson values to understand (With Simpson values, the lower the number, the higher the diversity)
+levels(sample_data(meta)$Day)
+meta$Day = factor(meta$Day, levels = c("D4", "D7"))  # Set the level order of values in "Day" column
+levels(sample_data(meta)$Day) #"D4" "D7"
+
+#Calculate the average shannon, invsimpson, numOTUs for each "All" subtype within meta
+shannon.invsimpson.numOTUs <- aggregate(meta[, 6:8], list(meta$All), mean)
+print(shannon.invsimpson.numOTUs)
+#Output (singletons removed):
+#        Group.1    numOTUS   shannon    invsimpson
+#1    D4_INFnm    79.66667    3.202696  11.903607
+#2 D4_NONINFnm    77.60000    3.117897   9.115051
+#3    D7_INFnm    94.80000    3.661901  19.286844
+#4 D7_NONINFnm    92.37500    3.770617  18.321736
+write.csv(shannon.invsimpson.numOTUs, file="FS9.shannon.invsimpson.num.OTUs.doubletons.txt", row.names=TRUE)
+
+#Shannon
+pairwise.wilcox.shannon.test <- pairwise.wilcox.test(meta$shannon, meta$All, p.adjust.method = 'none') #Calculate pairwise comparisons by "All" column of the shannon indices in "Shannon" column
+print(pairwise.wilcox.shannon.test) #Look at the results of 'pairwise.wilcox.shannon.test'
+
+#Inverse Simpson
+pairwise.wilcox.invsimpson.test <- pairwise.wilcox.test(meta$invsimpson, meta$All, p.adjust.method = 'none')
+print(pairwise.wilcox.invsimpson.test)
+
+#Generate a box and whisker plot of shannon (both shannon and inverse simpson diversity indices showed same trends: 
+#no significant differences between treatment groups within a day)
+shan <- ggplot(data = meta, aes(x=All, y=shannon, group=All, fill=Treatment)) +
+  geom_boxplot(position = position_dodge2(preserve = 'total')) +
+  facet_wrap(~Day, scales = 'free') +
+  scale_y_continuous(name = "Shannon diversity") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1),
+        strip.text.x = element_text(size=14),
+        axis.title.y = element_text(size=15)) +
+  scale_fill_manual(values = c(INFnm='#CC0066', NONINFnm='#56B4E9')) +
+  theme(legend.position = "none")
+shan
+# "free" within "facet_wrap" allows each plot to customize the scale to the specific data set (no forced scaling applied to all plots)
+# "position = position_dodge2(preserve = 'total')" fixes the ggplot box width, making them wider, prevents narrow boxes from forming in the plot
+
+#Save 'shan' as a .tiff for publication, 500dpi
+#ggsave("FS9_Shannon.tiff", plot=shan, width = 7, height = 7, dpi = 500, units =c("in"))
+
+#Generate a box and whisker plot of inverse simpson 
+invsimp <- ggplot(data = meta, aes(x=All, y=invsimpson, group=All, fill=Treatment)) +
+  geom_boxplot(position = position_dodge2(preserve = 'total')) +
+  facet_wrap(~Day, scales = 'free') +
+  scale_y_continuous(name = "Inverse Simpson diversity") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1),
+        strip.text.x = element_text(size=14),
+        axis.title.y = element_text(size=15)) +
+  scale_fill_manual(values = c(INFnm='#CC0066', NONINFnm='#56B4E9')) +
+  theme(legend.position = "none")
+invsimp
+#Save 'invsimp' as a .tiff for publication, 500dpi
+#ggsave("FS9_InverseSimpson.tiff", plot=invsimp, width = 7, height = 7, dpi = 500, units =c("in"))
+
+alphadiv <- plot_grid(shan, invsimp, labels = c('A', 'B'), label_size = 12) #How to fix plot dimensions so that y-axis labels doesn't overlap the plots?
+ggsave(alphadiv,
+       filename = './results/alpha_diversity_NONINFvsINF_D4D7.jpeg',
        width = 300,
        height = 120,
        device = 'jpeg',
--- scripts/FS9_phyloseq_Q1_additional.R
+++ scripts/FS9_phyloseq_Q1_additional.R
@@ -1,5 +1,5 @@
 #########################################
-#FS9 16S phyloseq - Noninfected vs infected, all days
+#FS9 16S phyloseq - Noninfected vs infected, additional analysis
 #Kathy Mou
 
 #NOTES: 
@@ -16,7 +16,7 @@ library(phyloseq)
 sessionInfo()
 #R version 3.6.3 (2020-02-29)
 
-#########################################
+######################################### All days
 
 #Read files for metadata and OTU table
 meta <- read.csv("./data/FS9_metadata_NONINFvINF_alldays.csv", row.names = 1)
@@ -72,8 +72,6 @@ dim(meta2) #79 5 (doubletons removed)
 SAM = sample_data(meta2, errorIfNULL = TRUE)
 head(SAM)
 dim(SAM) #79 5
-
-#Continue here!!!
 
 #Pull out OTU data from 'otu.meta2' dataframe
 head(otu.meta2[,1:10])
@@ -176,5 +174,174 @@ adonis.FS9
 # Day:Treatment  3    0.5699    0.18998   0.9693    0.03114   0.5125    
 # Residuals     71   13.9164    0.19601             0.76032           
 # Total         78   18.3033                        1.00000            
+# ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
+
+
+
+
+
+############################################################################### Days 4, 7 only
+
+
+#Read files for metadata and OTU table
+meta <- read.csv("./data/FS9_metadata_NONINFvINF_alldays.csv", row.names = 1)
+otu <- read.csv("./data/FS9.OTUtable.doubleton.csv", row.names=1)
+dim(otu) #1405 168
+head(otu[,165:168])
+meta3 <- meta[(meta$Day == "D4") | (meta$Day == "D7"), ]
+
+#Remove taxonomy from 'otu'
+tax <- otu[,(167:168)] #Removed column 168 "Taxonomy" and 167 to include the row names
+head(tax)
+colnames(tax)[1] <- "delete" #Renamed column 1 (formerly 167) as "delete" which will later be deleted
+head(tax)
+
+#Modify 'otu' with only OTU count data
+otu <- otu[,-168] #Remove column 168 "Taxonomy" to have only OTU data
+head(otu[,165:167]) 
+dim(otu) #1405 rows 167 columns
+
+#Transpose 'otu' to match format of 'meta'
+otu.trans <- t(otu) #Now rownames are sample names, columns are OTUs
+head(otu.trans[,1:5])
+class(meta) #dataframe
+class(otu) #dataframe
+
+#Merge 'otu' and 'meta' data frames
+otu.meta <- merge(meta3, otu.trans, by.x=0, by.y=0) 
+#x=0 means match via rownames from 'meta'; y=0 means match via rownames from 'otu.trans'
+head(otu.meta[,1:10])
+dim(otu.meta) #29 1410 (doubletons removed)
+rownames(otu.meta) <- otu.meta[,1] #Set column 1 as rownames for 'otu.meta'
+otu.meta <- otu.meta[,-1]
+class(otu.meta) #dataframe
+rownames(otu.meta)
+
+#Added an "All" column (combines 'Treatment' and 'Day' values) in new 'otu.meta2'
+otu.meta2<- cbind(otu.meta) #Make second copy of otu.meta to use to include "All" column
+colnames(otu.meta2)
+otu.meta2$All <- with(otu.meta2, paste0(Day, sep="_", Treatment)) #Create "All" column with Day and Treatment combined
+head(otu.meta2)
+dim(otu.meta2) #29 1410 (doubletons removed)
+head(otu.meta2[,1405:1410])
+head(otu.meta2[,1:10])
+otu.meta2<- otu.meta2[,c(1:4,1410,5:1409)] #Reorder columns to have "All" column after "Treatment" column
+#write.csv(otu.meta2, file="FS9.otu.meta_all.doubleton.csv")
+
+#Pull out metadata from 'otu.meta2' dataframe
+head(otu.meta2[,1:10])
+meta2 <- otu.meta2[,c(1:5)] #Take columns 1-5 ("Day" to "All") from 'otu.meta2' to make 'meta2'
+head(meta2)
+dim(meta2) #29 5 (doubletons removed)
+
+#Create SAM metadata table phyloseq object
+SAM = sample_data(meta2, errorIfNULL = TRUE)
+head(SAM)
+dim(SAM) #29 5
+
+#Pull out OTU data from 'otu.meta2' dataframe
+head(otu.meta2[,1:10])
+tail(otu.meta2[,1405:1410])
+otu2 <- otu.meta2[,c(6:1410)] #Select OTU columns to create 'otu2' dataframe
+head(otu2[,1:10])
+dim(otu2) #29 1405 (doubletons removed)
+otu2.trans <- t(otu2) #Transpose otu.all to have OTUs as rownames, sample names as column names
+head(otu2.trans[,1:10])
+dim(otu2.trans) #1405 29 (doubletons removed)
+
+#Merge 'tax' back into 'otu2.trans' for correct format and taxons
+head(tax)
+otu2.tax <- merge(otu2.trans, tax, by=0) #Merge by rownames aka OTU rownames
+dim(otu2.tax) #1405 32 (doubletons removed)
+head(otu2.tax[,1:10])
+head(otu2.tax[,25:32])
+row.names(otu2.tax) <- otu2.tax[,1] #Set first row of 'otu2.tax' as rownames
+head(otu2.tax[,1:5])
+otu2.tax <- otu2.tax[,-1] #Remove first row aka extraneous "Row.names" column from 'otu2.tax'
+head(otu2.tax[,1:5])
+
+#Split again
+dim(otu2.tax) #1405 31 (doubletons removed)
+head(otu2.tax[,20:31])
+otu2.notax <- otu2.tax[,1:29] #take rows 1-29 to make new dataframe 'otu2.notax' (30 is "delete" column, 31 is "Taxonomy" column)
+dim(otu2.notax) #1405 29 (doubletons removed)
+head(otu2.notax[,1:5])
+head(otu2.notax[,20:29])
+class(otu2.notax) #dataframe
+otu2.notax <- as.matrix(otu2.notax) #turn 'otu2.notax' into a matrix class
+class(otu2.notax) #matrix
+otu2.notax
+otu2.notax.trans <- t(otu2.notax)
+head(otu2.notax.trans[,1:10])
+
+#Create OTU table phyloseq object
+OTU = otu_table(otu2.notax.trans, taxa_are_rows = FALSE)
+head(OTU[,1:10])
+dim(OTU) #29 1405 (doubletons removed)
+class(OTU)
+OTU2 <- prune_taxa(taxa_sums(OTU) > 0, OTU)
+class(OTU2)
+head(OTU2[,1:10])
+taxa_sums(OTU2)
+dim(OTU2) #29 1156 (doubletons removed)
+
+#Edit taxonomy
+dim(otu2.tax) #1405 31 (doubletons removed)
+head(otu2.tax[,21:31])
+tax2 <- separate(data = otu2.tax, 
+                 col = Taxonomy, 
+                 into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep=";")
+#"separate" function separates "Taxonomy" column into 7 separate columns labeled "Kingdom", "Phylum", "Class", etc.
+head(tax2) #notice that Species column is blank
+dim(tax2) #1405 37 (doubletons removed)
+head(tax2[,30:37])
+tax2.kg <- tax2[,31:36] #Keep only taxonomy columns "Kingdom" to "Genus"
+head(tax2.kg)
+dim(tax2.kg) #1405 6 (doubletons removed)
+class(tax2.kg) #dataframe
+tax2.kg <- as.matrix(tax2.kg)
+class(tax2.kg) #matrix
+tax2.kg
+
+#Create TAX taxonomy table phyloseq object
+TAX = tax_table(tax2.kg)
+head(TAX)
+dim(TAX) #1405 6 (doubletons removed)
+
+#Create phyloseq object containing taxonomy, metadata, and otu table
+phyloseq.FS9 <- phyloseq(OTU2, SAM, TAX) #prune out any OTUs that have total to 0 in all samples
+phyloseq.FS9
+#otu_table()   OTU Table:         [ 768 taxa and 29 samples ]
+#sample_data() Sample Data:       [ 29 samples by 5 sample variables ]
+#tax_table()   Taxonomy Table:    [ 768 taxa by 6 taxonomic ranks ]
+
+save(phyloseq.FS9, file="phyloseq.FS9.doubleton.RData") #Use for FS9_alpha_beta_diversity.R when loading phyloseq.FS9 object
+
+###############################################################################
+
+#Distance calculation
+phyloseq.vegdist <- vegdist(phyloseq.FS9@otu_table, method="bray") 
+phyloseq.adonis <- as(sample_data(phyloseq.FS9), "data.frame")
+set.seed(1)
+adonis.FS9 <- adonis(phyloseq.vegdist~Day*Treatment, data=phyloseq.adonis, permutations=9999)
+adonis.FS9
+
+#Call:
+#adonis(formula = phyloseq.vegdist ~ Day * Treatment, data = phyloseq.adonis,      permutations = 9999) 
+
+#Permutation: free
+#Number of permutations: 9999
+
+#Terms added sequentially (first to last)
+
+#                Df SumsOfSqs   MeanSqs   F.Model   R2        Pr(>F)    
+# Day            1    0.5993    0.59930   3.9223    0.12682   0.0001 ***
+# Treatment      1    0.2039    0.20391   1.3346    0.04315   0.1603    
+# Day:Treatment  1    0.1028    0.10276   0.6726    0.02175   0.8365    
+# Residuals     25    3.8198    0.15279             0.80829           
+# Total         28    4.7258                        1.00000             
 # ---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
