2e83f7946e574168634f3e6f792362dadb175fb0
k39ajdM2 <kathy.mou@ars.usda.gov>
2020-12-15 19:18:29 -0600

    Added Q1 Phylum DNEG abundance plot script to go with DESeq2 results

--- scripts/FS9_PercentAbundance.R
+++ scripts/FS9_PercentAbundance.R
@@ -170,7 +170,6 @@ PhylumFig_D7
 
 write.csv(fobar.gather, file = "FS9_Phylum_OutDoubletons.csv")
 
-
 ###################################################### Order #####################################################
 FS9.order <- tax_glom(FS9, 'Order')
 order_tab <- as.data.frame(t(FS9.order@otu_table)) #Transpose 'FS9.order' by "otu_table"
@@ -306,7 +305,170 @@ OrderFig_D7
 write.csv(fobar.gather.order, file = "FS9_Order_OutDoubletons.csv")
 
 
-###################################################### Order: Day 7 only #####################################################
+#################################################### Q1 Phylum #####################################################
+FS9.phylum <- tax_glom(FS9, 'Phylum')
+phyla_tab <- as.data.frame(t(FS9.phylum@otu_table)) #Transpose 'FS9.phylum' by "otu_table"
+head(phyla_tab)
+FS9.phylum@tax_table[,2] #Second column is Phylum column
+colnames(phyla_tab) <- FS9.phylum@tax_table[,2] #Replace column names in phyla_tab from Otuxxxx with Phylum names
+phyla_tab2 <- phyla_tab/rowSums(phyla_tab) #Calculate the proportion of specific phyla per phyla column in 'phyla_tab'
+head(phyla_tab2)
+phyla_tab3 <- phyla_tab2[,colSums(phyla_tab2)>0.1] #Keep the columns that have greater than 0.1 value
+phyla_tab3$group <- rownames(phyla_tab3) #Rename rownames as "group"
+head(phyla_tab3)
+fobar <- merge(meta, phyla_tab3, by = 'group')
+head(fobar)
+fobar.gather <- fobar %>% gather(Phylum, value, -(group:All))  #Combines all phyla into one column and their respective % abundance values in "value" column
+#It added columns "group" through "All" before "Phylum" and "value"
+head(fobar.gather)
+
+#Reorder days in 'fobar.gather' plot
+levels(sample_data(fobar.gather)$Day) #"D0"    "D4"    "D7"    "DNEG3"
+fobar.gather$Day <- factor(fobar.gather$Day, levels=c("DNEG3", "D0", "D4","D7"))
+levels(sample_data(fobar.gather)$Day) #"DNEG3" "D0"    "D4"    "D7" 
+
+#Remove INFinject, INFfeed
+fobar.gather.phyla.q1 <- fobar.gather %>% 
+    select("group", "Day", "Pig", "Treatment", "Sample.type", "All", "Phylum", "value") %>% 
+    filter(Treatment != "INFfeed") %>% 
+    filter(Treatment != "INFinject")
+
+unique(fobar.gather.phyla.q1$Treatment) #"NONINFnm" "INFnm"
+
+#Count the number of unique items in 'fobar.gather'. We're interested in the total unique number of phylum
+fobar.gather.phyla.q1 %>% summarise(n_distinct(fobar.gather.phyla.q1$Phylum)) #13 total unique phyla
+fobar.gather.phyla.q1 <- fobar.gather.phyla.q1 %>% 
+    group_by(All) %>% 
+    mutate(value2=(value/(length(All)/13))*100) %>% #13 refers to number of Phyla
+    arrange((desc(value2))) %>% 
+    filter(value2 > 0) %>% 
+    mutate_if(is.numeric, round, digits = 4)%>% 
+    arrange(desc(value2)) %>% 
+    ungroup()
+
+#Phylum Figures
+#Day -3 Phylum
+DNEG3Phylum <- fobar.gather.phyla.q1 %>% 
+    subset(Day == "DNEG3") %>% 
+    group_by(Phylum) %>% 
+    select(Phylum, All) %>% 
+    count(All)
+#Decide which phyla to remove from plot that isn't present in all 4 treatment groups
+
+PhylumFig_DNEG3 <- fobar.gather.phyla.q1 %>% filter(Day == 'DNEG3') %>%
+    #select("group", "Day", "Pig", "Treatment", "Sample.type", "All", "Order", "value2") %>% 
+    #filter(Order %in% c("Mollicutes_RF39", "Verrucomicrobiales", "Coriobacteriales")) %>% 
+    ggplot(aes(x=Treatment, y=value2, group=All, fill=Phylum)) +
+    geom_boxplot(position = 'identity') +
+    geom_jitter(shape=21, width = .15)+
+    facet_wrap(~Phylum, scales = 'free') + 
+    ylab('Percent of Total Community') +
+    xlab ('') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Day -3") +
+    scale_fill_igv(name = "Phylum") +
+    theme(axis.text.x=element_text(angle=45, hjust=1),
+          axis.title.x = element_text(size=5),
+          legend.text = element_text(face = "italic"))
+PhylumFig_DNEG3
+
+
+OrderFig_D7_2 <- fobar.gather.order.2 %>% 
+    select("group", "Day", "Pig", "Treatment", "Sample.type", "All", "Order", "value2") %>% 
+    filter(Order %in% c("Mollicutes_RF39", "Verrucomicrobiales", "Coriobacteriales")) %>% 
+    ggplot(aes(x=Treatment, y=value2, group=All, fill=Order)) +
+    geom_boxplot(position = 'identity') +
+    geom_jitter(shape=21, width = .15)+
+    facet_wrap("Order", scales = "free") +
+    ylab('Percent of Total Community') +
+    xlab ('') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Day 7") +
+    theme(axis.text.x=element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          legend.text = element_text(face = "italic")) +
+    guides(fill= guide_legend(ncol = 1))
+OrderFig_D7_2
+
+
+
+#Day 0 Phylum
+D0Phylum <- fobar.gather %>% 
+    subset(Day == "D0") %>% 
+    group_by(Phylum) %>% 
+    select(Phylum, Treatment) %>% 
+    count(Treatment)
+#Decide which phyla to remove from plot that isn't present in all 4 treatment groups
+
+PhylumFig_D0 <- fobar.gather %>% filter(Day == 'D0'  & forplot == "keep") %>%
+    ggplot(aes(x=Treatment, y=value2, group=All, fill=Phylum)) +
+    geom_boxplot(position = 'identity') +
+    geom_jitter(shape=21, width = .15)+
+    facet_wrap(~Phylum, scales = 'free') + 
+    ylab('Percent of Total Community') +
+    xlab ('') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Day 0") +
+    scale_fill_igv(name = "Phylum") +
+    theme(axis.text.x=element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          legend.text = element_text(face = "italic"))
+PhylumFig_D0
+
+#Day 4 Phylum
+D4Phylum <- fobar.gather %>% 
+    subset(Day == "D4") %>% 
+    group_by(Phylum) %>% 
+    select(Phylum, Treatment) %>% 
+    count(Treatment)
+#Decide which phyla to remove from plot that isn't present in all 4 treatment groups
+
+PhylumFig_D4 <- fobar.gather %>% filter(Day == 'D4' & forplot == "keep") %>%
+    ggplot(aes(x=Treatment, y=value2, group=All, fill=Phylum)) +
+    geom_boxplot(position = 'identity') +
+    geom_jitter(shape=21, width = .15)+
+    facet_wrap(~Phylum, scales = 'free') + 
+    ylab('Percent of Total Community') +
+    xlab ('') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Day 4") +
+    scale_fill_igv(name = "Phylum") +
+    theme(axis.text.x=element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          legend.text = element_text(face = "italic"))
+PhylumFig_D4
+
+#Day 7 Phylum
+D7Phylum <- fobar.gather %>% 
+    subset(Day == "D7") %>% 
+    group_by(Phylum) %>% 
+    select(Phylum, Treatment) %>% 
+    count(Treatment)
+#Decide which phyla to remove from plot that isn't present in all 4 treatment groups
+
+PhylumFig_D7 <- fobar.gather %>% filter(Day == 'D7' & forplot == "keep") %>%
+    filter(Phylum != "Verrucomicrobia") %>% 
+    ggplot(aes(x=Treatment, y=value2, group=All, fill=Phylum)) +
+    geom_boxplot(position = 'identity') +
+    geom_jitter(shape=21, width = .15)+
+    facet_wrap(~Phylum, scales = 'free') + 
+    ylab('Percent of Total Community') +
+    xlab ('') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Day 7") +
+    scale_fill_igv(name = "Phylum") +
+    theme(axis.text.x=element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          legend.text = element_text(face = "italic"))
+PhylumFig_D7
+
+write.csv(fobar.gather, file = "FS9_Phylum_OutDoubletons.csv")
+
+
+
+
+
+###################################################### Q2 Day 7 Order only #####################################################
 
 FS9.order <- tax_glom(FS9, 'Order')
 order_tab <- as.data.frame(t(FS9.order@otu_table)) #Transpose 'FS9.order' by "otu_table"
