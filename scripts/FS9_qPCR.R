0d569a985556fb43bd25f9e7116513579daa4b41
k39ajdM2 <kathy.mou@ars.usda.gov>
2020-12-23 15:13:11 -0600

    update with summary stats and tukey hsd test, cowplot of tet32 and tetW

--- scripts/FS9_qPCR.R
+++ scripts/FS9_qPCR.R
@@ -20,15 +20,88 @@ sessionInfo()
 tet32 <- read.csv('./data/FS9_tet32_qPCR_results.csv', stringsAsFactors = FALSE)
 tetw <- read.csv('./data/FS9_tetW_qPCR_results.csv', stringsAsFactors = FALSE)
 
-tet32stats <- tet32 %>% group_by(Day,Treatment) %>% summarise(mean=mean(value), 
-                                                                   n=n(), 
-                                                                   sd=sd(value), 
-                                                                   se=sd/n) %>% write_csv('./results/tet32_mean.csv')
+str(tet32) #see 
 
-tet32 %>% 
+#Two-way ANOVA with repeated measures, Tukey multiple pairwise-comparisons: 
+#http://www.sthda.com/english/wiki/two-way-anova-test-in-r#multiple-pairwise-comparison-between-the-means-of-groups
+
+#Two way ANOVA
+tet32aov <- aov(log10tet32 ~ Treatment * Day, data=tet32) 
+summary(tet32aov)
+#               Df Sum Sq Mean Sq F value   Pr(>F)    
+#Treatment      2   3.42   1.711   2.383    0.0989 .  
+#Day            2  20.19  10.093  14.058    6.11e-06 ***
+#Treatment:Day  4   8.41   2.103   2.930    0.0260 *  
+#Residuals     78  56.00   0.718                     
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#These results would lead us to believe that the levels of Day, and Day + Treatment interaction
+#are associated with significant different mean log10tet32 abundances.
+#In addition, the interaction between Day and Treatment indicates that the relationship between mean log10tet32 abundances
+#and Treatment depend on the day sampled.
+
+tetWaov <- aov(log10tetW ~ Treatment * Day, data=tetw)
+summary(tetWaov)
+#```````````````Df Sum Sq Mean Sq F value   Pr(>F)    
+#Treatment      2   6.88   3.440   5.110    0.00821 ** 
+#Day            2  18.05   9.025  13.408    9.88e-06 ***
+#Treatment:Day  4  11.16   2.791   4.146    0.00425 ** 
+#Residuals     78  52.50   0.673                     
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#These results would lead us to believe that the levels of Day or Treatment, and Day + Treatment interaction
+#are associated with significant different mean log10tet32 abundances.
+#In addition, the interaction between Day and Treatment indicates that the relationship between mean log10tet32 abundances
+#and Day depend on the type of Treatment administered.
+
+#Tukey multiple pairwise-comparisons
+TukeyHSD(tet32aov, which = "Treatment:Day")
+#can't save as csv so I copied the results in console to 
+#tet32_qPCR_summarystats_Tukey.xlsx
+TukeyHSD(tetWaov, which = "Treatment:Day")
+#can't save as csv so I copied the results in console to
+#tetW_qPCR_summarystats_Tukey.xlsx
+
+#Summary stats
+tet32stats <- tet32 %>% group_by(Day,Treatment) %>% summarise(mean=mean(log10tet32), 
+                                                                   n=n(), 
+                                                                   sd=sd(log10tet32), 
+                                                                   se=sd/n) %>% write_csv('./results/tet32_qPCR_mean.csv')
+
+tetWstats <- tetw %>% group_by(Day,Treatment) %>% summarise(mean=mean(log10tetW),
+                                                            n=n(),
+                                                            sd=sd(log10tetW),
+                                                            se=sd/n) %>% write_csv('./results/tetW_qPCR_mean.csv')
+
+tet32fig <- tet32 %>% 
   ggplot(aes(x=Day, y=log10tet32, color=Treatment)) +
   scale_color_manual(values = c(INFinject='#E69F00', INFnm='#CC0066', INFfeed='#999999')) +
   geom_boxplot() + 
+  geom_jitter(position=position_jitterdodge(jitter.width = .20)) +
+  theme(axis.text.x=element_text(color = 'black', size = 14),
+        axis.text.y=element_text(color = 'black', size=14)) + 
   ylab('log10-fold change of tet32 gene abundance') +
-  #axis label font size! and axis tick font size!
   theme_bw()
+
+tetWfig <- tetw %>% 
+  ggplot(aes(x=Day, y=log10tetW, color=Treatment)) +
+  scale_color_manual(values = c(INFinject='#E69F00', INFnm='#CC0066', INFfeed='#999999')) +
+  geom_boxplot() + 
+  geom_jitter(position=position_jitterdodge(jitter.width = .20)) +
+  theme(axis.text.x=element_text(color = 'black', size = 14),
+        axis.text.y=element_text(color = 'black', size=14)) + 
+  ylab('log10-fold change of tetW gene abundance') +
+  theme_bw()
+
+#Combine tet32 and tetW graphs and save combined figure
+fig8 <- plot_grid(tet32fig, tetWfig, labels = c('A', 'B'), label_size = 12)
+fig8
+ggsave(fig8,
+       filename = './Fig8_tet32tetW_qPCR_INFfeedINFinjectINFnm.jpeg',
+       width = 180,
+       height = 120,
+       device = 'jpeg',
+       dpi = 300,
+       units = 'mm')
