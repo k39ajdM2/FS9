0fd6ae8b29d54df4aa5d6cdae61d3de77aa01ace
k39ajdM2 <kathy.mou@ars.usda.gov>
2021-03-05 08:59:50 -0600

    update figures for Q2

--- scripts/FS9_Q2_NMDS_DeSeq2_Order.R
+++ scripts/FS9_Q2_NMDS_DeSeq2_Order.R
@@ -86,7 +86,7 @@ NMDS_ellipse <- function(metadata, OTU_table, grouping_set,
   }
 }
 
-################################################
+################################################ NMDS ################################################
 #Run FS9_phyloseq_Q2.R to generate phyloseq.FS9 object to run the R script below.
 
 #Setting up 'phyloseq' into dataframes for NMDS calculation
@@ -126,7 +126,6 @@ levels(df_ell$Day) #"D7"  "D11" "D14"
 levels(metanmds$Day) #"D7"  "D11" "D14"
 
 #All points, all days, all treatments, samples that aren't relevant are grayed out on plot
-#All points, all days, all treatments, samples that aren't relevant are grayed out on plot
 #Plotting with gridlines and axes, gray points for All days
 metanmds.2 <- metanmds
 metanmds.2$Treatment2 = metanmds.2$Treatment
@@ -148,50 +147,175 @@ nmdsplot_treatment2<- ggplot(metanmds, aes(x=MDS1, y=MDS2)) +  annotate(x=metanm
   scale_color_manual(values = c(INFinject='#00BA38',
                                 INFnm='#F8766D',
                                 INFfeed='#619CFF')) +
-  labs(caption = 'Ordination stress = 0.182', color="Treatment group")
+  labs(color="Treatment")
 nmdsplot_treatment2
 
-#Make magnitude of change plot
+##################################################### DESeq2 ######################################################################
 
-#Purpose: This code plots the F-statistic from PERMANOVA pairwise comparisons of INFnm to 
-#either INFinject or INFfeed groups, over time (displays the magnitude of change in the fecal bacterial community structure 
-#of the two antibiotic treatment groups relative to infected control)
+otu <- import_mothur(mothur_shared_file = './data/stability.outdoubletons.abund.opti_mcc.shared') #use unrarified data
+taxo <- import_mothur(mothur_constaxonomy_file = './data/stability.outdoubletons.abund.opti_mcc.0.03.cons.taxonomy')
+meta <- read.table(file = './data/FS9_metadata_INF_InjectFeedNM.csv', sep = ',', header = TRUE)
 
-#Files needed:
-#FS9_Q2_MagnitudeOfChange.csv
+#Organize meta file
+rownames(meta) <- meta$Sample
+meta <- meta[,-1] #remove Sample column
+meta$Set <- paste(meta$Day, meta$Treatment, sep = '_') #Make a set column
 
-#The F-values were obtained from FS9.WithinDayPairwiseComparisons.Q2.doubletons.txt data generated in the "Beta diversity" section
-#FS9_Q2_MagnitudeOfChange.csv was created from FS9.WithinDayPairwiseComparisons.Q2.doubletons.txt data that was rearranged
-#To make the FS9_Q2_MagnitudeOfChange.csv file, open FS9.WithinDayPairwiseComparisons.Q2.doubletons.txt file 
-#(created from FS9_alpha_beta_diversity_Q2.R) in excel, 
-#copy columns "F. Model" through "p.adjusted2" and paste in a separate spreadsheet. 
-#Add "Day" and "Treatment" columns and save as "FS9_Q2_MagnitudeOfChange.csv".
+#Make phyloseq object SRD129 (combine taxonomy, OTU, and metadata)
+phy_meta <- sample_data(meta) 
+FS9 <- phyloseq(otu, taxo)
+FS9 <- merge_phyloseq(FS9, phy_meta)   # combines the metadata with this phyloseq object
+colnames(tax_table(FS9))
+colnames(tax_table(FS9)) <- c('Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus')
 
-fecal <- read.csv("./data/FS9_Q2_MagnitudeOfChange.csv")
-class(fecal)
-fecal$Day <- factor(fecal$Day) #Encode "Day" as a factor
-fecal$p.adjusted <- round(fecal$p.adjusted, 3)
-fecal2 <- ggplot(data=fecal, aes(x=Day, y=F.Model, group=Treatment)) +
-  #geom_line(aes(color=Treatment)) + 
-  geom_point(aes(color=Treatment, size = 10)) +
-  ylab("PERMANOVA F vs INFnm") +
-  scale_fill_manual(values = c(INFinject='#00BA38',
-                               INFnm='#F8766D',
-                               INFfeed='#619CFF')) +
-  scale_color_manual(values = c(INFinject='#00BA38',
-                                INFnm='#F8766D',
-                                INFfeed='#619CFF')) +
-  geom_label_repel(aes(label=p.adjusted), box.padding = 0.35, point.padding=0.5,segment.color = 'grey50', size = 5) +
-  theme_classic(base_size = 12) +
-  theme(axis.text.y = element_text(size = 16), axis.text.x = element_text(size=16), axis.title.x = element_text(size=20), axis.title.y = element_text(size=20), legend.text=element_text(size=16), legend.title=element_text(size=16)) +
-  labs(color="Treatment group") +
-  scale_size(guide = 'none') +
+FS9
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:          [ 2039 taxa and 73 samples ]
+#sample_data() Sample Data:       [ 73 samples by 5 sample variables ]
+#tax_table()   Taxonomy Table:    [ 2039 taxa by 6 taxonomic ranks ]
+
+#Prune
+FS9 <- prune_samples(sample_sums(FS9) > 1300, FS9)  # This removes samples that have fewer than 1300 sequences associated with them.
+#Jules used 1000. You used 1300 because Day 0 Treatment group 2 (room 2) only has 5 samples and subsampling at 2000 would take out 
+#one of the samples.
+FS9 <- prune_taxa(taxa_sums(FS9) > 10, FS9)        # removes OTUs that occur less than 10 times globally
+tax_table(FS9) [1:5, 1:6] #see what's in tax_table first 5 rows, first 6 columns
+
+# If you want to group OTUs uncomment the tax_glom() line and select your desired taxrank
+# right now all these analysis are done at the OTU level.
+
+#Grouping OTUs by desired taxonomic level using the tax_glom function 
+FS9.order <- tax_glom(FS9, taxrank = "Order")
+# This method merges species that have the same taxonomy at a certain taxanomic rank. 
+# Its approach is analogous to tip_glom, but uses categorical data instead of a tree. 
+
+##################################################### Day 14 ######################################################################
+
+sample_data(FS9.order)
+
+FS9.D14 <- subset_samples(FS9.order, Day == 'D14')
+sample_sums(FS9.D14)
+colnames(otu_table(FS9.D14)) #check on all the sample names
+FS9.D14 <- prune_taxa(taxa_sums(FS9.D14) > 1, FS9.D14)
+#if taxa_sums is >1, then it will print that out in FS9.D14 object and not include anything with <1.
+rowSums(FS9.D14@otu_table)
+
+######### 1. Day 14 INFinject vs INFnm ###################
+
+#INFnm = I
+#INFinject= J
+#INFfeed = O
+
+meta$Set
+#Number of pigs per group (using meta2 dataframe): 
+sum(meta$Set == "D14_INFnm")
+#14
+sum(meta$Set == "D14_INFinject")
+#19
+
+sample_data(FS9.D14)$Set <- factor(sample_data(FS9.D14)$Set,
+                                   levels =c("D14_INFinject", 'D14_INFnm', "D14_INFfeed"))
+FS9.D14.De <- phyloseq_to_deseq2(FS9.D14, ~ Set)
+FS9.D14.De <- DESeq(FS9.D14.De, test = "Wald", fitType = "parametric")
+FS9.D14.De$Set
+resultsNames(FS9.D14.De)
+#[1] "Intercept"        "Set_D14_INFnm_vs_D14_INFinject"   "Set_D14_INFfeed_vs_D14_INFinject"
+res.D14.ji = lfcShrink(FS9.D14.De, coef = "Set_D14_INFnm_vs_D14_INFinject", type = 'apeglm')
+sigtab.D14.ji = res.D14.ji[which(res.D14.ji$padj < .05), ]
+sigtab.D14.ji = cbind(as(sigtab.D14.ji, "data.frame"), as(tax_table(FS9.D14)[rownames(sigtab.D14.ji), ], "matrix"))
+sigtab.D14.ji$newp <- format(round(sigtab.D14.ji$padj, digits = 3), scientific = TRUE)
+sigtab.D14.ji$Treatment <- ifelse(sigtab.D14.ji$log2FoldChange >=0, "Depleted in INFinject", "Enriched in INFinject")
+head(sigtab.D14.ji) 
+
+deseq.D14.ji <- 
+  ggplot(sigtab.D14.ji, aes(x=reorder(rownames(sigtab.D14.ji), log2FoldChange), y=log2FoldChange, fill = Treatment)) +
+  geom_bar(stat='identity') + geom_text(aes(x=rownames(sigtab.D14.ji), y=3, label = paste(Order)), size=5, fontface = 'italic')+ labs(x="Order")+
+  theme(axis.text.x=element_text(color = 'black', size = 13),
+        axis.text.y=element_text(color = 'black', size=13), 
+        axis.title.x=element_text(size = 12),
+        axis.title.y=element_text(size = 12))+ ggtitle('Differentially Abundant Order\n in INFinject Group Relative to INFnm\n in Fecal Microbiota on Day 14')+ coord_flip() +
+  theme(plot.title = element_text(size = 14, hjust=0.5), legend.text = element_text(size=12), legend.title = element_text(size=13)) +
+  scale_fill_manual(values = c('Enriched INFinject'='#00BA38',
+                               'Depleted in INFinject'='#F8766D'))
+deseq.D14.ji
+
+#Add OTU and comparisons columns
+sigtab.D14.ji
+sigtab.D14.ji$OTU <- rownames(sigtab.D14.ji)
+sigtab.D14.ji
+sigtab.D14.ji$comp <- 'D14_INFnm_vs_INFinject'
+
+#Create final significant comparisons table
+final.sigtab.ji <- sigtab.D14.ji
+
+
+######### 2. Day 14 INFfeed vs INFnm ###################
+
+#INFnm = I
+#INFinject= J
+#INFfeed = O
+
+meta$Set
+#Number of pigs per group (using meta2 dataframe): 
+sum(meta$Set == "D14_INFnm")
+#14
+sum(meta$Set == "D14_INFfeed")
+#13
+
+#Extract results from a DESeq analysis, organize table
+sample_data(FS9.D14)$Set <- factor(sample_data(FS9.D14)$Set,
+                                   levels =c( "D14_INFfeed", 'D14_INFnm',"D14_INFinject"))
+FS9.D14.De <- phyloseq_to_deseq2(FS9.D14, ~ Set)
+FS9.D14.De <- DESeq(FS9.D14.De, test = "Wald", fitType = "parametric")
+FS9.D14.De$Set
+resultsNames(FS9.D14.De)
+#"Set_D14_INFnm_vs_D14_INFfeed"     "Set_D14_INFinject_vs_D14_INFfeed"
+res.D14.oi = lfcShrink(FS9.D14.De, coef = "Set_D14_INFnm_vs_D14_INFfeed", type = 'apeglm')
+sigtab.D14.oi = res.D14.oi[which(res.D14.oi$padj < .05), ]
+sigtab.D14.oi = cbind(as(sigtab.D14.oi, "data.frame"), as(tax_table(FS9.D14)[rownames(sigtab.D14.oi), ], "matrix"))
+format(sigtab.D14.oi$padj, scientific = TRUE)
+sigtab.D14.oi$newp <- format(round(sigtab.D14.oi$padj, digits = 3), scientific = TRUE)
+sigtab.D14.oi$Treatment <- ifelse(sigtab.D14.oi$log2FoldChange >=0, "Depleted in INFfeed", "Enriched in INFfeed")
+head(sigtab.D14.oi) 
+
+deseq.D14.oi <- 
+  ggplot(sigtab.D14.oi, aes(x=reorder(rownames(sigtab.D14.oi), log2FoldChange), y=log2FoldChange, fill = Treatment)) +
+  geom_bar(stat='identity') + geom_text(aes(x=rownames(sigtab.D14.oi), y=3, label = paste(Order)), size=5, fontface = 'italic')+ labs(x="Order")+
+  theme(axis.text.x=element_text(color = 'black', size = 13),
+        axis.text.y=element_text(color = 'black', size=13), 
+        axis.title.x=element_text(size = 12),
+        axis.title.y=element_text(size = 12))+ ggtitle('Differentially Abundant Order\n in INFfeed Group Relative to INFnm\n in Fecal Microbiota on Day 14')+ coord_flip() +
+  theme(plot.title = element_text(size = 14, hjust=0.5), legend.text = element_text(size=12), legend.title = element_text(size=13)) +
+  scale_fill_manual(values = c('Enriched INFfeed'='#619CFF',
+                               'Depleted in INFfeed'='#F8766D'))
+deseq.D14.oi
+
+#Add OTU and comparisons columns
+sigtab.D14.oi
+sigtab.D14.oi$OTU <- rownames(sigtab.D14.oi)
+sigtab.D14.oi
+sigtab.D14.oi$comp <- 'D14_INFfeed_vs_INFnm'
+
+#Create final significant comparisons table
+final.sigtab <- rbind(final.sigtab.ji, sigtab.D14.oi)
+
+# Create plot containing all significant orders with log2-fold changes > 0.25 for Q2 DeSeq2 results
+deseqfinalplot <- ggplot(final.sigtab, aes(x=reorder(rownames(final.sigtab), log2FoldChange), y=log2FoldChange, fill = Treatment)) +
+  geom_bar(stat='identity') + geom_text(aes(x=rownames(final.sigtab), y=3, label = paste(Order)), size=5, fontface = 'italic')+ labs(x="Order")+
+  theme(axis.text.x=element_text(color = 'black', size = 13),
+        axis.text.y=element_text(color = 'black', size=13), 
+        axis.title.x=element_text(size = 12)) +
+  coord_flip() +
+  theme(plot.title = element_text(size = 14, hjust=0.5), legend.text = element_text(size=12), legend.title = element_text(size=13)) +
+  scale_fill_manual(values = c('Depleted in INFfeed'='dodgerblue1', 'Depleted in INFinject'='green3')) +
   theme_bw()
-fecal2
+#light green for depleted in INFinject
+#light blue for INFfeed
+#color name options for ggplot: https://www.nceas.ucsb.edu/sites/default/files/2020-04/colorPaletteCheatsheet.pdf
+deseqfinalplot
 
-#Cowplot of Q1_NONINFnm_INFnm Order and Phylum DNEG3, D7
-Fig6ab <- plot_grid(fecal2, nmdsplot_treatment2, labels = c('A', 'B'), label_size = 12, ncol=1)
-#, rel_widths = c(1, 4)
-Fig6ab
+#Cowplot of NMDS with DESeq2
+Fig4ab<- plot_grid(nmdsplot_treatment2, deseqfinalplot, labels = c('A', 'B'), label_size = 18, ncol=1)
+Fig4ab
 
-ggsave("Q2_INFnm_inject_feed_MagnitudeOfChange_NMDS.tiff", plot=Fig6ab, width = 10, height = 9, dpi = 500, units =c("in"))
+ggsave("Q2_INFnm_inject_feed_NMDS_DESeq2.tiff", plot=Fig4ab, width = 10, height = 9, dpi = 500, units =c("in"))
